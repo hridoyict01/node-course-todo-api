b03b8f546474e432febb11ad9627d70a60c4453a
Hridoy4 <Hridoy4@DESKTOP-9CDLVUQ>
2018-03-18 12:46:35 +0600

    Add tests for PATCH/todos/:id

--- Server/models/users.js
+++ Server/models/users.js
@@ -1,4 +1,5 @@
-var mongoose=require('mongoose');
+ const mongoose=require('mongoose');
+ const validator=require('validator');
 
 var User=mongoose.model('User',{
   name:{
@@ -11,8 +12,30 @@ var User=mongoose.model('User',{
     type:String,
     trim:true,
     required: true,
-    minlenght:1
-  }
+    minlenght:1,
+    unique:true,
+    validate:{
+      validator:(value)=>{
+        return validator.isEmail(value);
+      },
+      message:'{VALUE} is not a valid email address'
+    }
+  },
+  password:{
+    type:String,
+    required:true,
+    minlenght:6
+  },
+  tokens:[{
+    access:{
+      type:String,
+      required:true
+    },
+    token:{
+      type:String,
+      required:true
+    }
+  }]
 });
 
 module.exports={User};
--- Server/server.js
+++ Server/server.js
@@ -1,80 +1,101 @@
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
-var express=require('express');
-var bodyParser=require('body-parser');
-var {ObjectID}=require('mongodb');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/users');
 
-var {mongoose}=require('./db/mongoose');
-var {Todo}=require('./models/todo');
-var {User}=require('./models/users');
-
-var app=express();
-var port=process.env.PORT || 3000;
+var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
-    var todo= new Todo({
-      text:req.body.text
-    });
-    todo.save().then((doc)=>{
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
     res.send(doc);
-  },(e)=>{
+  }, (e) => {
     res.status(400).send(e);
   });
 });
 
-app.get('/todos',(req,res)=>{
-  Todo.find().then((todos)=>{
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
     res.send({todos});
-  },(e)=>{
+  }, (e) => {
     res.status(400).send(e);
   });
 });
 
-//GET/todos/id
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
-app.get('/todos/:id',(req,res)=>{
-  var id=req.params.id;
-  if(!ObjectID.isValid(id)){
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  Todo.findById(id).then((todo)=>{
-    if(!todo){
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
       return res.status(404).send();
     }
+
     res.send({todo});
-  }).catch((e)=>{
+  }).catch((e) => {
     res.status(400).send();
   });
-
 });
 
-app.delete('/todos/:id',(req,res)=>{
-  //get the id
-  var id=req.params.id;
-  //validate the id
-  if(!ObjectID.isValid(id)){
-    //not valid return 404
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  //remove todo by id
-  Todo.findByIdAndRemove(id).then((todo)=>{
-    if(!todo){
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
       return res.status(404).send();
     }
+
     res.send({todo});
-  }).catch ((e)=>{
+  }).catch((e) => {
     res.status(400).send();
   });
-
 });
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
 
-app.listen(port,()=>{
-  console.log(`Started up at port: ${port}`);
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
-module.exports={app};
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+module.exports = {app};
