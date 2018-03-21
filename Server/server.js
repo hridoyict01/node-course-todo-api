6cf8f842827e204d0d4a80ba05a0b804b8beed3d
Hridoy4 <Hridoy4@DESKTOP-9CDLVUQ>
2018-03-21 13:42:07 +0600

    Add POST/users/login route

--- Server/server.js
+++ Server/server.js
@@ -116,6 +116,20 @@ app.post('/users', (req, res) => {
     res.send(req.user);
   });
 
+//POST/users/login{login,password}
+
+  app.post('/users/login',(req,res)=>{
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email,body.password).then((user)=>{
+      user.generateAuthToken().then((token)=>{
+        res.header('x-auth', token).send(user);
+      });
+    }).catch((e)=>{
+      res.status(400).send();
+    });
+  });
+
 
 
 
